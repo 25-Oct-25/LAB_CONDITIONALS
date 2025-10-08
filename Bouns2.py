5abafdef1927befcee4e6a231d94c65bef31acf8
Amwaj <Amwaj.m.zahrani@gmail.com>
2025-10-08 14:36:19 +0300

    resolve Bouns2

--- Bouns2.py
+++ Bouns2.py
@@ -1,11 +1,11 @@
 username=input("Enter your username: ")
 email=input("Enter your email: ")
+checkEmail=email.partition("@gmail")
 
-if len(username)>2 and email.find("@gmail")!=-1 :
-    print(f"welcome {username}, you registered with the email {email}!")
-elif email.find("@gmail")==-1:
+if len(username)<=2: 
+    print("the name length must be more than 2 characters, please provide a valid name.") 
+elif len(checkEmail[0])>2 and checkEmail[0].isidentifier() and checkEmail[1]=="@gmail" and checkEmail[2]==".com":
+        print(f"welcome {username}, you registered with the email {email}!")
+elif len(checkEmail[0])<=2 or checkEmail[0].isidentifier()==False or checkEmail[1]!="@gmail" or checkEmail[2]==".com":
     print(" the email is not valid , please provide a valid email .")
-elif len(username)<=2: 
-    print("the name length must be more than 2 characters, please provide a valid name.")    
-else: 
-    print("please provide a valid name and email.")
+   
