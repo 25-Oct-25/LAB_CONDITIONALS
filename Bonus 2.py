167ea22efd1f6d584bb9a1da8209fc461b8aab3b
test <1osama1adel1@gmail.com>
2025-10-08 14:34:18 +0300

    solved lab

--- Bonus 2.py
+++ Bonus 2.py
@@ -1,15 +1,58 @@
-name = input("Enter your name :")
-email = input("Enter your email")
+while True:
+    name = input("Enter your name: ").strip()
+    if len(name) > 2:
+        break
+    print("Name must be more than 2 characters.\n")
 
+while True:
+    email = input("Enter your email: ").strip()
 
-if len(name) <= 2 : 
-    print("the name length must be more than 2 characters, please provide a valid name.")
+    if any(ch.isspace() for ch in email):
+        print("Email must not contain spaces or whitespace.\n")
+        continue
 
-if "@gmail.com" in email : 
-    print("valid email")
-else :
-    print("the email is not valid , please provide a valid email.")
+    
+    if email.count("@") != 1:
+        print("Email must contain exactly one '@'.\n")
+        continue
 
+    local, domain = email.rsplit("@", 1)
 
-if len(name) > 2 and "@gmail.com"in email : 
-    print(f"Welcome {name},you registered with the email {email}!O")
+    
+    if domain.lower() != "gmail.com":
+        print("Domain must be gmail.com.\n")
+        continue
+
+    
+    if not local:
+        print("Email must have characters before '@'.\n")
+        continue
+
+   
+    s = local.lower()
+
+    
+    valid_chars = True
+    for c in s:
+        if c == '.':
+            continue
+        if not (c.isascii() and c.isalnum()): # isascii for : ASCII   isalnum for : A–Z/a–z/0–9
+            valid_chars = False
+            break
+
+    if not valid_chars:
+        print("Username may contain only letters, digits, and dots.\n")
+        continue
+
+    if s[0] == '.' or s[-1] == '.':
+        print("Username cannot start or end with a dot.\n")
+        continue
+
+    if '..' in s:
+        print("Username cannot contain consecutive dots.\n")
+        continue
+
+   
+    break
+
+print(f"Welcome {name}, you registered with the email {email}!")
