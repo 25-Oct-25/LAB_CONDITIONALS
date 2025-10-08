a3baceb6345a97cffc858916e76f97c3f4feec05
Norah <norahwasr@gmail.com>
2025-10-08 22:28:48 +0300

    lab_conditionals solved

---
+++ lab_conditionals.py
@@ -0,0 +1,18 @@
+movie = "Taken"
+
+rating = 3
+
+popularityScore = 72.65
+
+
+if rating >= 4 and popularityScore > 80:
+    print("Highly recommended")
+
+elif rating >= 3 and popularityScore > 70:
+    print("I recommended it. It is good")
+
+elif rating <= 2 and popularityScore > 60:
+    print("You should check it out!")  
+
+else:
+    print("Don't watch it. It is a waste of time")
