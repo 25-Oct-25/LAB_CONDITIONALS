7b8445b1e2150f7c7d795697a0491904741f6a90
Rakan Sedran <srakan595@gmail.com>
2025-10-08 21:42:21 +0300

    rakan

--- bonus2.py
+++ bonus2.py
@@ -2,8 +2,22 @@ name = input("Enter your name:")
 email = input("Enter your email:")
 if len(name) <= 2 :
     print("Name must be more than 2 characters.")
-else :
-   if "@" in email and email.endswith("@gmail.com") :
-       print(f"welcome {name}, you registered with the email {email} !")
-   else:
-        print( "the email is not valid , please provide a valid email .")
+else:
+       
+    if  "@" not in email :
+        print("the email is not valid , please provide a valid email .")           
+    else:
+        username, domain = email.split("@", 1) 
+   
+        if " " in username :
+            print("the email is not valid , please provide a valid email .") 
+        elif username[0] in ".-" :
+            print("the email is not valid , please provide a valid email .") 
+        elif username[-1] in ".-" :
+            print("the email is not valid , please provide a valid email .") 
+        elif ".." in username :
+            print("the email is not valid , please provide a valid email .") 
+        elif domain != "gmail.com" :
+            print("the email is not valid , please provide a valid email .")
+        else:
+            print(f"Welcome {name}, you registered with the email {email}")
