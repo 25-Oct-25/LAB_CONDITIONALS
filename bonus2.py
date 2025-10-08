8ee641f01b03638d425eb8b706434362d75bed8b
Ghala <ghalanmasi@icloud.com>
2025-10-08 13:49:30 +0300

    Fix Email Validation

--- bonus2.py
+++ bonus2.py
@@ -1,13 +1,13 @@
 name = input("Enter your Name: ")
 email =  input("Enter your Email: ")
 
-if len(name) <= 2 and not "@gmail" in email:
+if len(name) <= 2 and (not "@gmail" in email or not email.endswith(".com") or " " in email):
     print("Both name and email are invalid, please provide valid information.")
 
 elif len(name) <= 2 :
     print("The name length must be more than 2 characters, please provide a valid name.")
 
-elif not "@gmail" in email :
+elif not "@gmail" in email or not email.endswith(".com") or " " in email :
     print("The email is not valid , please provide a valid email .")
 
 else:
