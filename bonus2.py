ed83473791c086b38d4eb951b5c1cef3811c7c57
Ghala <ghalanmasi@icloud.com>
2025-10-08 13:09:15 +0300

    I have finished working on bonus2

---
+++ bonus2.py
@@ -0,0 +1,14 @@
+name = input("Enter your Name: ")
+email =  input("Enter your Email: ")
+
+if len(name) <= 2 and not "@gmail" in email:
+    print("Both name and email are invalid, please provide valid information.")
+
+elif len(name) <= 2 :
+    print("The name length must be more than 2 characters, please provide a valid name.")
+
+elif not "@gmail" in email :
+    print("The email is not valid , please provide a valid email .")
+
+else:
+     print(f"Welcome {name}, you registered with the email {email} !")
