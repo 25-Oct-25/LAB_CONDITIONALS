80fc61030dae1fb17f575e5f4aa4ba19c4a4fc04
Maha Saud <al-maha2244@hotmail.com>
2025-10-08 14:35:16 +0300

    modify email verification

--- bonus2.py
+++ bonus2.py
@@ -8,7 +8,7 @@ else:
     print("the name length must be more than 2 characters, please provide a valid name.")
 
 # Check Email:
-if Email.count("@") == 1 and Email.endswith("@gmail.com"):
+if Email.count("@") == 1 and Email.endswith("@gmail.com") and Email.index("@") > 0:
     print("welcome" , Name , "you registered with the" , Email )
 else:
     print("the email is not valid , please provide a valid email.")
