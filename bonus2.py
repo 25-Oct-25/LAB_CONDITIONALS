a09f2a588abbd2206cf6c40b33536061ea0a7784
444101792 <444101792@student.ksu.edu.sa>
2025-10-08 13:52:50 +0300

    fixed the code

--- bonus2.py
+++ bonus2.py
@@ -1,14 +1,22 @@
 ##نواف البريكان
-
+## الكود يلخبط
 name = input("Enter your name: ")
 email = input("Enter your email: ")
 flag = False
+
 if len(name)> 2:
     flag = True
-if "@gmail" in email and "." in email:
+at = email.find("@")
+gmail = email[at+1:at+6]
+if (len(email)>10) and (not email.startswith("@")) and (not email.endswith("@")) and ("@" in email) and (email.count("@")==1) and (email.endswith(".com")) and (gmail == "gmail"):
     flag = True
+else:
+    flag = False
+
 if flag:
     print(f"Welcome {name}, you registered with the email {email} !")
 
 else:
     print("the email is not valid , please provide a valid email .")
+
+print(gmail)
