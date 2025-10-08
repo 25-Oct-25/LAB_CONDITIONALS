3b799736dbcb601f1affb1548409e6ed1c68d90e
Ziyad <zsyz8335@gmai.com>
2025-10-08 12:43:16 +0300

    Ziyad Alzahrani

--- Bonus2.py
+++ Bonus2.py
@@ -1,11 +1,14 @@
-name:str = input("Enter your name: ")
-email:str = input("Enter your email: ")
+name = input("Enter your name: ")
+email = input("Enter your email: ")
 
-if len(name) > 2 and "@gmail" in email:
-    print(f"Welcome {name}, your registred email is {email}")
-elif len(name) <= 2 and "@gmail" in email:
-    print("the name length must be more than 2 characters, please provide a valid name.")
-elif "@gmail" not in email and len(name) > 2:
-    print(" the email is not valid , please provide a valid email .")
-elif len(name) <= 2 and "@gmail" not in email:
-    print("the email and the name are not valid , please provide a valid email and name .")
+if len(name) > 2 and email.endswith("@gmail.com"):
+    print(f"Welcome {name}, you registered with the email {email}!")
+
+elif len(name) <= 2 and email.endswith("@gmail.com"):
+    print("The name length must be more than 2 characters, please provide a valid name.")
+
+elif not email.endswith("@gmail.com") and len(name) > 2:
+    print("The email is not valid, please provide a valid email.")
+
+else:
+    print("The email and the name are not valid, please provide a valid email and name.")
