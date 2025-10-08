2d94f9b47755384a2616546782f9c0c361dcc035
lama <leema442@gmail.com>
2025-10-08 14:12:36 +0300

    Modified

--- bouns2.py
+++ bouns2.py
@@ -3,11 +3,15 @@ name=input("Please enter your name: ")
 
 email =input("Please enter your email: ")
 
-if len(name)>2 and email.find("@gmail.com") != -1 :
-    print(f"Welcome {name}, you registered with the email {email} !")
-elif len(name)<= 2:
+if len(name)>2:
+    if email.find("@gmail.com") != -1 or email.find("outlook.com") != -1 or email.find("hotmail.com") != -1:
+        if email.find(" ") == -1 and email.find("@@") == -1:
+            print(f"Welcome {name}, you registered with the email {email} !")
+
+          
+if len(name)<= 2:
     print("The name length must be more than 2 characters, please provide a valid name")
-elif email.find("@gmail.com") == -1 :
+if email.find("@gmail.com") == -1 or email.find("@outlook") == -1 or email.find("@hotmail.com") == -1 or email.find(" ")!= -1 or email.find("@@") != -1:
     print("The email is not valid , please provide a valid email .")
 
   
