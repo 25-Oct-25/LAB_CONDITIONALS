7a5fa1dd656d93ca13e72a846d9a35f13ec3a6fb
Hind <newhind2023@gmail.com>
2025-10-08 14:08:38 +0300

    update bonus2

--- bonus2.py
+++ bonus2.py
@@ -1,9 +1,25 @@
+
 name = input("Enter your name: ")
 email = input("Enter your email: ")
+allowed_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789._-"
 
-if len(name) > 2 and "@gmail.com" in email:
-    print(f"Welcome {name}, you registered with the email {email} !")
-elif len(name) <= 2:
-    print("The name length must be more than 2 characters, please provide a valid name.")
+if len(name) <2 :
+    print("name length must be more than 2 characters")
 else:
-    print("The email is not valid, please provide a valid email.")
+    if (
+        " " in email or 
+        email.count("@") != 1 or
+        not email.endswith("@gmail.com") or
+        email.startswith("@")
+    ): 
+        print("The email is not valid")
+    else:
+        firstName = email.split("@")[0]
+        vaild = all(i in allowed_chars for i in firstName)
+
+        if not vaild :
+            print("The email is not valid, please provide a valid email.")
+        else:
+            print(f"Welcome {name}, you registered with the email {email}!")
+
+
