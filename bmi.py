77d775892932d3e96fdb53108df7f0cd8e14215c
Sana <sanaalboshi77@gmail.com>
2025-10-08 17:26:00 +0300

    sana2

--- bmi.py
+++ bmi.py
@@ -3,7 +3,7 @@ wieght = float(input("Enter your wieght wiegth :"))
 height = float(input("Enter your  height :"))
 
 bmi = wieght / (height ** 2)
-print("Your BMI is:", round(bmi, 2)) 
+print("Your BMI is:", round(bmi, 1)) 
 
 if bmi > 25 :
     print("You are overwieght you need to work out more and watch your diet .")
--- user.py
+++ user.py
@@ -1,9 +1,28 @@
-name = input("Enter your name :")
-email = input("Enter you email :")
+name = input("Enter your name :").strip()
+email = input("Enter you email :").strip()
+allowed_domains = ("@gmail.com", "@yahoo.com", "@hotmail.com")
 
-if len(name) <= 2 :
-    print("The name length must be more than 2 characters, please provide a valid name.")
-elif email.endswith ("@gmail.com"):
-    print(f"Welcome {name} , you registered with the email {email} ")
+
+if not name:
+    print("Name cannot be empty.")
+
+elif len(name) <= 2 :
+    print("The name length must be more than 2 characters .")
+
+elif not name[0].isalpha():
+    print("The name must start with a letter.")
+
+elif  "*" in name or "&" in name or "%" in name or "#" in name or "@"  in name:
+    print("The name cannot contain *, &, % ,or # characters.")
+
+elif not email:
+    print("Email cannot be empty.")
+
+elif " " in email:
+    print("Email cannot contain spaces.")
+
+elif email.endswith(allowed_domains):
+    print(f"Welcome {name}, you registered with the email {email}")
+
 else :
-    print("The email is not valid , please provide a valid email")
+    print("The email is not valid .")
