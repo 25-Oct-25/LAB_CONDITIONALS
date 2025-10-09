12974f282daa344d25bcd555b3740425868736c0
Hind <newhind2023@gmail.com>
2025-10-09 10:03:59 +0300

    lastUpdateBonus2

--- bonus2.py
+++ bonus2.py
@@ -3,23 +3,21 @@ name = input("Enter your name: ")
 email = input("Enter your email: ")
 allowed_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789._-"
 
-if len(name) <2 :
-    print("name length must be more than 2 characters")
+if len(name) <= 2:
+    print("The name length must be more than 2 characters")
 else:
     if (
-        " " in email or 
+        " " in email or
         email.count("@") != 1 or
         not email.endswith("@gmail.com") or
         email.startswith("@")
-    ): 
+    ):
         print("The email is not valid")
     else:
         firstName = email.split("@")[0]
-        vaild = all(i in allowed_chars for i in firstName)
+        valid = all(ch in allowed_chars for ch in firstName)
 
-        if not vaild :
-            print("The email is not valid, please provide a valid email.")
+        if not valid or firstName == "":
+            print("The email is not valid")
         else:
             print(f"Welcome {name}, you registered with the email {email}!")
-
-
